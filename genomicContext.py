40b56cbbaaa2a0d8e171b54b2b8677f0954751f8
raphael-upmc <raphael.meheust@gmail.com>
2018-01-03 16:37:11 -0800

    the script should be able to compute pairwise occurrence counting

--- genomicContext.py
+++ genomicContext.py
@@ -59,64 +59,74 @@ def randomizingfamiliesOrder() :
     return randomizedFamiliesOrder
 
 
+def orf2familyFunction() :
+    orf2family = dict()
+    orf2bin_filename = '/data7/proteinfams/3.6k.PF/3600genomes.4pub.all.prot_bin_subFam_fam_clan.tab'
+    file = open(orf2bin_filename,'r')
+    for line in file :
+        line = line.rstrip()
+        orfName,genome,subfamily,family,clan = line.split("\t")
+        orf2family[orfName] = family
+    file.close()
+    return orf2family
 
+def genome2familyOrder(familySet,genomeSet) :
+    orf2family = orf2familyFunction()
+    feature_filename = ""
+    genome2scaffold2strand2geneList = dict()
+    for line in file :
+        line = line.rstrip()
+        orfName,genome,scaffold,start,end,strand = line.split("\t")
 
-family_set = set()
-file = open('coreFamiliesAnnotation.tsv','r')
-header = next(file)
-for line in file :
-    line = line.rstrip()
-    liste = line.split('\t')
-    family = liste[1]
-    module = liste[0]
-    if module != 'specificNonCprBacteriaCore' :
-        family_set.add(family)
-file.close()
-print(family_set)
-print(len(family_set))
-
-scaffold2orfs = defaultdict(set)
-genome2scaffold2families = dict()
-orf2bin_filename = '/data7/proteinfams/3.6k.PF/3600genomes.4pub.all.prot_bin_subFam_fam_clan.tab'
-file = open(orf2bin_filename,'r')
-for line in file :
-    line = line.rstrip()
-    orfName,genome,subfamily,family,clan = line.split("\t")
-    scaffold = '_'.join(orfName.split('_')[:-1])
-    
-    if family in family_set :
-        scaffold2orfs[scaffold].add(orfName)
-        if genome in genome2scaffold2families :
-            genome2scaffold2families[ genome ][ scaffold ].add(family)
+        if len(genomeSet) != 0 and genome not in genomeSet :
+            continue
+        
+        if orfName not in orf2family :
+            family = None
         else :
-            genome2scaffold2families[ genome ] = defaultdict(set)
-            genome2scaffold2families[ genome ][ scaffold ].add(family)
-file.close()
-
-
-pair2weight = defaultdict(int)
-for genome,scaffold2families in genome2scaffold2families.items() :
-    print(genome)
-    for scaffold,familyList in sorted(scaffold2families.items(),key=lambda x:len(x[1]),reverse=True) :
-        for family1 in familyList :
-            for family2 in familyList :
-                if family1 == family2 :
-                    continue
-                key = '_'.join(sorted([family1,family2]))
-                pair2weight[ key ] += 1
-
-output = open('scaffoldCooccurrence.txt','w')
-output.write('\t'+'\t'.join(list(family_set))+'\n')
-for family1 in family_set :
-    output.write(family1)
-    for family2 in family_set :
-        if family1 == family2 :
-            output.write('\t'+'NA')
-        else :
-            key = '_'.join(sorted([family1,family2]))
-            if key in pair2weight :
-                output.write('\t'+str(pair2weight[key]/2))
+            if len(familySet) != 0 and orf2family[orfName] not in familySet :                
+                family = None
             else :
-                output.write('\t'+'0')
-    output.write('\n')
-output.close()
+                family = orf2family[orfName]
+
+                    
+        if genome not in genome2scaffold2strand2geneList :
+            genome2scaffold2strand2geneList[genome] = dict()
+
+        if scaffold not in genome2scaffold2strand2geneList[genome] :
+            genome2scaffold2strand2geneList[genome][scaffold] = defaultdict(list)
+
+        genome2scaffold2strand2geneList[genome][scaffold][strand].append([ int(start) , int(end) , family ])
+    file.close()
+    return genome2scaffold2strand2geneList
+
+
+
+
+
+if __name__ == "__main__":
+
+    genomeSet = set()    
+    familySet = set()
+    file = open('coreFamiliesAnnotation.tsv','r')
+    header = next(file)
+    for line in file :
+        line = line.rstrip()
+        liste = line.split('\t')
+        family = liste[1]
+        module = liste[0]
+        if module != 'specificNonCprBacteriaCore' :
+            familySet.add(family)
+    file.close()
+    print(familySet)
+    print(len(familySet))
+
+    genome2scaffold2strand2geneList = genome2familyOrder(familySet,genomeSet)
+
+    pair2weight = defaultdict(int)
+    k=5
+    for genome,scaffold2strand2geneList in genome2scaffold2strand2geneList.items() :
+        for scaffold,strand2geneList in scaffold2strand2geneList.items() :
+            for strand,geneList in strand2geneList.items() :
+                kNearestNeighbors(geneList,k,pair2weight)
+    
