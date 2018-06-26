0678f19b5e4857dd223d310b0bf6a78ce9605f7d
raphael-upmc <raphael.meheust@gmail.com>
2018-06-26 16:41:46 -0700

    creating a wrapper to run signalP on large protein dataset

---
+++ signalP.py
@@ -0,0 +1,100 @@
+#! /usr/bin/python
+
+""" a wrapper to run signalP on large protein dataset """
+
+import os,sys,re
+from collections import defaultdict
+from Bio import SeqIO
+from Bio.SeqRecord import SeqRecord
+
+
+def runSignalP(fasta_filename,output_filename,log_filename) :
+    cmd = "/data7/proteinfams/SignalP/signalp-4.1/signalp -f short -t gram- "+fasta_filename+" > "+output_filename+" 2>>"+log_filename
+    print(cmd)
+    status = os.system(cmd)
+    return status
+
+
+
+
+initial_fasta_filename = sys.argv[1]
+final_output_filename = sys.argv[2]
+cpu = int(sys.argv[3])
+
+
+if os.path.exists('target_tmp') :
+    sys.exit('target_tmp already exists, remove it first')
+else :
+    os.mkdir('target_tmp')
+    os.mkdir('target_tmp/fasta')
+    os.mkdir('target_tmp/log')
+    os.mkdir('target_tmp/output')
+    
+
+###################
+# splitting fasta #
+###################
+    
+# Signalp allow no more than 10000 sequences as inpout, so we have to split the dataset into fasta bins of <10,000 sequences !
+
+cpt2defline = dict()
+binSeq = 10000
+cpt = nb = 0
+binNb = 1
+output_fasta_filename = "target_tmp/fasta"+"/"+str(binNb)+".faa"
+output = open(output_fasta_filename,"w")
+for seq_record in SeqIO.parse(fasta_filename , "fasta") :
+    cpt += 1
+    nb += 1
+    cpt2defline[ str(nb) ] = record.id
+    if cpt < binSeq : 
+        SeqIO.write( SeqRecord(seq=seq_record.seq,id=str(nb),description="") ,output,"fasta")
+    else :
+        output.close()
+        binNb += 1
+        cpt = 1
+        output_fasta_filename = "psort_tmp/fasta"+"/"+str(binNb)+".fasta"
+        output = open(output_fasta_filename,"w")
+        SeqIO.write( SeqRecord(seq=seq_record.seq,id=str(nb),description="")  ,output,"fasta")
+output.close()
+
+
+###################
+# running signalP #
+###################
+
+
+results = list()
+pool = mp.Pool(processes=cpu,maxtasksperchild=1) # start 20 worker processes and 1 maxtasksperchild in order to release memory
+for root, dirs, files in os.walk("target_tmp/fasta"):
+    for filename in files :
+        fasta_filename = root+'/'+filename
+        output_filename = "target_tmp/output"+'/'+filename.replace('.faa','.signalP')
+        log_filename = "target_tmp/log"+'/'+filename.replace('.faa','.log')
+        results.append( pool.apply_async( runSignalP, args= (fasta_filename,output_filename,log_filename) ))
+pool.close() # Prevents any more tasks from being submitted to the pool
+pool.join() # Wait for the worker processes to exit
+
+
+
+####################
+# writting signalP #
+####################
+
+output = open(final_output_filename , 'w' )
+
+for root, dirs, files in os.walk("target_tmp/output"):
+    for filename in files :
+        file = open(root+'/'+filename,"r")
+        for line in file :
+            line = line.rstrip()
+            if re.match("#",line) :
+                output.write(line+"\n")
+            else :
+
+                liste = line.split()
+                defline = cpt2defline[int(liste[0])]
+                result = liste[9]
+                output.write(defline+"\t"+result+"\n")
+        file.close()
+output.close()
