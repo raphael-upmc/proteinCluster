beaf53513ccad374bfb45c85ae4db20be0a30c04
raphael-upmc <raphael.meheust@gmail.com>
2018-09-17 16:48:58 -0700

    improving splitingFasta

--- splitingFasta.py
+++ splitingFasta.py
@@ -4,41 +4,60 @@
 import os,sys,re
 from collections import defaultdict
 from Bio import SeqIO
-
-fasta_filename = sys.argv[1]
-nb = int(sys.argv[2])
+import argparse
 
 
-if os.path.exists('fasta') :
-    sys.exit('psort_tmp already exists, remove it first')
-else :
-    os.mkdir('fasta')
 
+if __name__ == "__main__":
     
+    parser = argparse.ArgumentParser(description='splitting a fasta file into k files')
+    parser.add_argument('fasta_filename', help='the path of the FASTA_FILE')
+    parser.add_argument('nb',type=int,help='the number of sub fasta files you want to create')
 
-cpt = 0
-for seq_record in SeqIO.parse(fasta_filename , "fasta") : # get the number of sequences
-    cpt += 1
+    args = parser.parse_args()
+    
+    if os.path.exists(args.fasta_filename) :
+        fasta_filename = os.path.abspath(args.fasta_filename)
+    else:
+        sys.exit(args.fasta_filename+' does not exist, exit')
 
+    nb = args.nb
+    if nb < 1 :
+        sys.exit('error! '+str(nb)+' must be a positive')
 
-###################
-# splitting fasta #
-###################
-
-binSeq = ( cpt / nb ) + 1
-cpt = 0
-binNb = 1
-output_fasta_filename = "fasta"+"/"+str(binNb)+".fasta"
-output = open(output_fasta_filename,"w")
-for seq_record in SeqIO.parse(fasta_filename , "fasta") :
-    cpt += 1
-    if cpt < binSeq : 
-        SeqIO.write(seq_record,output,"fasta")
+    if os.path.exists('fasta') :
+        sys.exit('fasta already exists, remove it first')
     else :
-        output.close()
-        binNb += 1
-        cpt = 1
-        output_fasta_filename = "fasta"+"/"+str(binNb)+".fasta"
-        output = open(output_fasta_filename,"w")
-        SeqIO.write(seq_record,output,"fasta")
-output.close()
+        os.mkdir('fasta')
+
+    print('Fasta filename: '+fasta_filename)
+    print('Number of bins: '+str(nb))
+
+    cpt = 0
+    for seq_record in SeqIO.parse(fasta_filename , "fasta") : # get the number of sequences
+        cpt += 1
+    print('\nNumber of sequences in '+fasta_filename+': '+str(cpt)+'\n')
+
+    ###################
+    # splitting fasta #
+    ###################
+
+    binSeq = ( cpt / nb ) + 1
+    cpt = 0
+    binNb = 1
+    output_fasta_filename = "fasta"+"/"+str(binNb)+".faa"
+    output = open(output_fasta_filename,"w")
+    for seq_record in SeqIO.parse(fasta_filename , "fasta") :
+        if cpt < binSeq :
+            cpt += 1
+            SeqIO.write(seq_record,output,"fasta")
+        else :
+            output.close()
+            print('Bin: '+str(binNb)+'\t'+str(cpt)+' sequences')
+            binNb += 1
+            cpt = 1
+            output_fasta_filename = "fasta"+"/"+str(binNb)+".faa"
+            output = open(output_fasta_filename,"w")
+            SeqIO.write(seq_record,output,"fasta")
+    output.close()
+    print('Bin: '+str(binNb)+'\t'+str(cpt)+' sequences')
