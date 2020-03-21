1bc6569a337c5519038831cb00012cb260603f8b
raphael-upmc <raphael.meheust@gmail.com>
2020-03-20 17:52:08 -0700

    adding argparse

--- downloadingNcbiAssemblies.py
+++ downloadingNcbiAssemblies.py
@@ -5,6 +5,7 @@ import os,sys,re
 from collections import defaultdict
 import urllib.request
 from datetime import date
+import argparse
 
 def functionTaxId2taxonName() :
     taxId2taxName = dict()
@@ -58,7 +59,7 @@ def downloadingAssemblySummary(assembly_summary_filename) :
         liste = urllib.request.urlretrieve(url, assembly_summary_filename)
         #print(liste)
 
-def readingAssemblySummary(assembly_summary_filename,taxId2taxName,taxId2parent,taxaSet,accession2gtdb) :
+def readingAssemblySummary(assembly_summary_filename,taxId2taxName,taxId2parent,taxaSet,accessionSet,accession2gtdb) :
     accession2filename = dict()
     accession2ncbiTaxonomy = dict()
     file = open(assembly_summary_filename,'r')
@@ -80,6 +81,11 @@ def readingAssemblySummary(assembly_summary_filename,taxId2taxName,taxId2parent,
         else:
             lineage_gtdb = 'Na'
         genome_filename = ftp_path+'/'+basename+'_genomic.fna.gz'
+
+        if accession in accessionSet :
+            accession2filename[accession] = genome_filename
+            accession2ncbiTaxonomy[accession] = lineage_ncbi
+        
         for taxon in taxaSet :
             if re.search(';'+taxon+';',lineage_gtdb) or re.search(','+taxon+',',lineage_ncbi):
                 #print(accession+'\t'+lineage_gtdb+'\t'+lineage_ncbi)
@@ -101,9 +107,21 @@ def readingListTaxa(filename) :
     file.close()
     return taxaSet
 
+def readingAccessionList(filename) :
+    accessionSet = set()
+    file = open(filename,'r')
+    for line in file :
+        accession = line.rstrip()
+        if re.match(r'#',accession) :
+            continue
+        accessionSet.add(accession)
+    file.close()
+    return accessionSet
+
+
 def readingGtdb() :
     accession2gtdb = dict()
-    bacteria_filename = 'bac120_metadata.tsv'
+    bacteria_filename = '/home/meheurap/scripts/proteinCluster/bac120_metadata.tsv'
     file = open(bacteria_filename,'r')
     #header = next(file)
     for line in file :
@@ -122,58 +140,98 @@ def readingGtdb() :
 
 
 
+if __name__ == "__main__":
 
+    parser = argparse.ArgumentParser(description='downloading genome assemblies from NCBI')
+    parser.add_argument('directory', help='the path of the DIRECTORY')
+    parser.add_argument('output_summary_filename',help='the path of the OUTPUT_SUMMARY_FILE, results will be stored in this file')
+    parser.add_argument('--accessionList',help='list of accessions')    
+    parser.add_argument('--taxaList',help='list of taxa')    
 
-filename = 'taxaList'
-taxaSet = readingListTaxa(filename)
-
-today = date.today()
-assembly_summary_filename = 'assembly_summary_genbank_'+str(today)+'.txt'
-downloadingAssemblySummary(assembly_summary_filename)
-
-print('loading taxDump database...')
-taxId2taxName,taxName2taxId = functionTaxId2taxonName()
-taxId2taxRank,taxId2parent = functionTaxId2taxon()
-print('done')
-
-accession2gtdb = readingGtdb()
-
-accession2filename,accession2ncbiTaxonomy = readingAssemblySummary(assembly_summary_filename,taxId2taxName,taxId2parent,taxaSet,accession2gtdb)
-print('number of assemblies: '+str(len(accession2filename)))
-
-
-output_filename = 'accession2taxonomy.txt'
-output = open(output_filename,'w')
-output.write('assembly\tncbiTaxonomy\tgtdbTaxonomy\tftpUrl\n')
-directory = '/groups/banfield/users/meheurap/Paula/genomes'
-cpt = 0
-accessionError = set()
-for accession,genome_filename in accession2filename.items() :
-    cpt += 1
-    output_genome_filename = directory+'/'+accession+'.fna.gz'
-    if accession in accession2ncbiTaxonomy :
-        ncbiTaxonomy = accession2ncbiTaxonomy[accession]
+    args = parser.parse_args()
+    
+    if os.path.exists(args.directory) :
+        directory = os.path.abspath(args.directory)
     else:
-        ncbiTaxonomy = 'Na'
+        sys.exit(args.directory+' does not exist, exit')
 
-    if accession in accession2gtdb :
-        gtdbTaxonomy = accession2gtdb[accession]
-    else:
-        gtdbTaxonomy = 'Na'
+    output_summary_filename = os.path.abspath(args.output_summary_filename)
+
+    if args.taxaList != None :
+        if os.path.exists(args.taxaList) :
+            taxaList_filename = os.path.abspath(args.taxaList)
+        else:
+            sys.exit(args.taxaList+' does not exist, exit')
+
+    if args.accessionList != None :
+        if os.path.exists(args.accessionList) :
+            accessionList_filename = os.path.abspath(args.accessionList)
+        else:
+            sys.exit(args.accessionList+' does not exist, exit')
+
+
+    print('directory: '+directory)
+    print('output_summary_filename: '+output_summary_filename)
+    print('accessionList_filename: '+accessionList_filename)
+    print('taxaList_filename: '+taxaList_filename)
+
+    taxaSet = set()
+    if os.path.exists(taxaList_filename) :
+        taxaSet = readingListTaxa(taxaList_filename)
+
+    accessionSet = set()
+    if os.path.exists(accessionList_filename) :
+        accessionSet = readingAccessionList(accessionList_filename)
+    
+    today = date.today()
+    assembly_summary_filename = 'assembly_summary_genbank_'+str(today)+'.txt'
+    downloadingAssemblySummary(assembly_summary_filename)
+
+    print('loading taxDump database...')
+    taxId2taxName,taxName2taxId = functionTaxId2taxonName()
+    taxId2taxRank,taxId2parent = functionTaxId2taxon()
+    print('done')
+
+    accession2gtdb = readingGtdb()
+
+    accession2filename,accession2ncbiTaxonomy = readingAssemblySummary(assembly_summary_filename,taxId2taxName,taxId2parent,taxaSet,accessionSet,accession2gtdb)
+    print('number of assemblies: '+str(len(accession2filename)))
+
+
+    output_filename = 'accession2taxonomy.txt'
+    output = open(output_filename,'w')
+    output.write('assembly\tncbiTaxonomy\tgtdbTaxonomy\tftpUrl\n')
+
+    cpt = 0
+    accessionError = set()
+    for accession,genome_filename in accession2filename.items() :
+        cpt += 1
+        output_genome_filename = directory+'/'+accession+'.fna.gz'
+        if accession in accession2ncbiTaxonomy :
+            ncbiTaxonomy = accession2ncbiTaxonomy[accession]
+        else:
+            ncbiTaxonomy = 'Na'
+
+        if accession in accession2gtdb :
+            gtdbTaxonomy = accession2gtdb[accession]
+        else:
+            gtdbTaxonomy = 'Na'
         
-    output.write(accession+'\t'+ncbiTaxonomy+'\t'+gtdbTaxonomy+'\t'+genome_filename+'\n')
-    if os.path.exists(output_genome_filename) :
-        continue
-    liste = urllib.request.urlretrieve(genome_filename, output_genome_filename)
-    print( str(cpt)+'\t'+str(liste[0])+'\t'+str(liste) )
+        output.write(accession+'\t'+ncbiTaxonomy+'\t'+gtdbTaxonomy+'\t'+genome_filename+'\n')
+        if os.path.exists(output_genome_filename) :
+            continue
+        if os.path.exists(output_genome_filename.replace('.gz','')) :
+            continue
 
-    if not os.path.exists(output_genome_filename) :
-        accessionError(accession)
+        liste = urllib.request.urlretrieve(genome_filename, output_genome_filename)
+        print( str(cpt)+'\t'+str(liste[0])+'\t'+str(liste) )
 
-output.close()
+        if not os.path.exists(output_genome_filename) :
+            accessionError(accession)
+
+    output.close()
 
 
-
-print(str(len(accessionError))+' assemblies were not downloaded')
-for accession in accessionError :
-    print(accession+'\t'+accession2filename[ accession ])
+    print(str(len(accessionError))+' assemblies were not downloaded')
+    for accession in accessionError :
+        print(accession+'\t'+accession2filename[ accession ])
